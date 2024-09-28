8fa731acf445523df8fc1724488facc257f5c295
SurajSJagtap1 <surajsjagtap2022@gmail.com>
2024-09-28 20:14:50 +0530

    answer inputs validated

--- ratings_generator.rb
+++ ratings_generator.rb
@@ -13,12 +13,22 @@ class RatingsGenerator
 
   private
 
-  def collect_answers # Asks and validates user's answers for the survey
+  def collect_answers # Asks questions and collects user's answers for the survey
+    answers = []
     QUESTIONS.each_key do |question_key|
-      print "#{QUESTIONS[question_key]}\n"
-      ans = gets.chomp
-      answers.push(ans)
+      print "#{QUESTIONS[question_key]}:\n"
+      ans = gets.chomp.downcase
+      answers.push(ans) if is_answer_valid?(question_key, ans)
     end
+    answers
+  end
+
+  def is_answer_valid?(question_key, ans)  # Validates user's answers for a particular question
+    return true if ALLOWED_ANSWERS.include?(ans)
+    puts "Please select your answer from following options:  #{ALLOWED_ANSWERS}\n(Case Insensitive answers are FINE)\n"
+    print "\n#{QUESTIONS[question_key]}: "
+    ans = gets.chomp.downcase
+    is_answer_valid?(question_key, ans)   # Recursive call to the same method until user inputs the appropriate answer
   end
 
   def calculate_rating_for_current_run(answers)
