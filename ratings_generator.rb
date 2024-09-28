1bd6eef07fd595a9dd72ecb6959be27bd702c27a
SurajSJagtap1 <surajsjagtap2022@gmail.com>
2024-09-28 19:36:10 +0530

    modularised current rating method and answer collection

--- ratings_generator.rb
+++ ratings_generator.rb
@@ -6,20 +6,24 @@ store = PStore.new(STORE_NAME)
 
 class RatingsGenerator
   def do_prompt
-    # Ask each question and get an answer from the user's input.
-    answers = []
+    answers = collect_answers
+    rating_for_current_run = calculate_rating_for_current_run(answers)
+    puts "Your Rating for this survey is: #{rating_for_current_run}\n"
+  end
 
+  private
+
+  def collect_answers # Asks and validates user's answers for the survey
     QUESTIONS.each_key do |question_key|
       print "#{QUESTIONS[question_key]}\n"
       ans = gets.chomp
       answers.push(ans)
     end
-    puts "Your Rating for this survey is: #{calculate_rating_for_current_run(answers)}\n"
   end
 
   def calculate_rating_for_current_run(answers)
     yes_count = answers.select{|answer|  answer == 'yes' || answer == 'y'}.count
-    ( 100 * yes_count / QUESTIONS.keys.count)
+    ( 100 * yes_count / QUESTIONS.keys.count )
   end
 
   def do_report
