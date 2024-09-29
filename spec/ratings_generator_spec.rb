c6b55862b4a871be0361d19e0748fb8e872c20ca
SurajSJagtap1 <surajsjagtap2022@gmail.com>
2024-09-29 23:06:00 +0530

    specs WIP

--- spec/ratings_generator_spec.rb
+++ spec/ratings_generator_spec.rb
@@ -1,7 +1,7 @@
-require_relative '../ratings_generator'
+# require_relative '../ratings_generator'
 
-RSpec.describe RatingsGenerator do
-    it 'should ask questions and calculate ratings based on answers' do
-        RatingsGenerator.new.do_prompt
-    end
-end
+# RSpec.describe RatingsGenerator do
+#     it 'should ask questions and calculate ratings based on answers' do
+#         RatingsGenerator.new.do_prompt
+#     end
+# end
